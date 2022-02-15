db156149ff0f0adbc156c14bb1f1129d3556920b
raj23689 <rajmajumdar236@gmail.com>
2022-02-15 07:24:07 +0530

    added isogram file and ignore

---
+++ isogram.c
@@ -0,0 +1,16 @@
+#include <stdbool.h>
+
+bool isIsogram(char *str) {
+    int bs = 0;  // bit set
+    char c;
+    while ((c = *str++) != '\0') {
+        if (c >= 'A' && c <= 'Z')
+            c -= 'A';
+        else if (c >= 'a' && c <= 'z')
+            c -= 'a';
+        else continue;
+        if ((bs & (1 << c)) > 0) return false;
+        bs |= (1 << c);
+    }
+    return true;
+}
